6420faa2f537efdd77821763f4b6e7c8c73bffec
Ann-Louis Lilja <lilja.annlouis@gmail.com>
2025-11-27 09:42:07 +0100

    Squashed 'eureka-server/' content from commit 60c82c9
    git-subtree-dir: eureka-server
    git-subtree-split: 60c82c989852e2fce66adfe130021fc9d6311fe3

---
+++ src/test/java/com/edufy/eurekaserver/EdufyEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edufy.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdufyEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
