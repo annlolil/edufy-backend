89706f7ce04797ca85613fc188f47c722499ec37
Ann-Louis Lilja <lilja.annlouis@gmail.com>
2025-11-27 09:40:23 +0100

    Squashed 'edufy-gateway/' content from commit 037200b
    git-subtree-dir: edufy-gateway
    git-subtree-split: 037200b179cc1fdeff42a00abe7ad291e75ba6d2

---
+++ src/main/java/org/example/edufygateway/config/SecurityConfig.java
@@ -0,0 +1,65 @@
+package org.example.edufygateway.config;
+
+import org.example.edufygateway.converter.JwtAuthConverter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final JwtAuthConverter jwtAuthConverter;
+
+    @Autowired
+    public SecurityConfig(JwtAuthConverter jwtAuthConverter) {
+        this.jwtAuthConverter = jwtAuthConverter;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/actuator/**").permitAll()
+                .requestMatchers("/api/v1/admin/**").hasRole("edufy_ADMIN")
+                    .anyRequest().authenticated()
+            )
+            .oauth2Login(oauth2 -> oauth2
+                    .defaultSuccessUrl("/", true))
+            .oauth2Client(oauth2 -> {})
+            .oauth2ResourceServer(oauth2 ->
+                    oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthConverter)));
+
+
+
+        return http.build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withIssuerLocation("http://81.231.105.129:8084/realms/edufyrealm")
+                .build();
+    }
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtGrantedAuthoritiesConverter grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        grantedAuthoritiesConverter.setAuthoritiesClaimName("resource_access.edufy.roles");
+        grantedAuthoritiesConverter.setAuthorityPrefix("ROLE_");
+
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(grantedAuthoritiesConverter);
+        return jwtAuthenticationConverter;
+    }
+
+
+}
---
+++ src/test/java/org/example/edufygateway/EdufyGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.edufygateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdufyGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
